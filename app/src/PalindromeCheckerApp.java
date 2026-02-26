b22e8ab9d7b78b8a654824a6cc8ccde61096f64d
srijal-sharma <itzsrijal21@gmail.com>
2026-02-26 14:21:40 +0530

    Print a Hardcoded Palindrome Result

---
+++ app/src/PalindromeCheckerApp.java
@@ -0,0 +1,11 @@
+//*
+// *-------MAIN CLASS UC2 PalindromeCheckerApp-----------
+// UC2- Hardcoded palindrome validation-------*/
+public class PalindromeCheckerApp {
+    public static void main(String args[]){
+        String input="madam";
+
+
+
+    }
+}
