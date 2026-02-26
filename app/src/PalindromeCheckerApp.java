535a5dd1c98287d0ef856906b706463bb2a4e258
srijal-sharma <itzsrijal21@gmail.com>
2026-02-26 15:58:52 +0530

    UC3:Palindrome Check Using String Reverse

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,31 +1,33 @@
-//*
-// *-------MAIN CLASS UC2 PalindromeCheckerApp-----------
-// UC2- Hardcoded palindrome validation-------*/
+/*
+ *UC3
+ *@author Srijal
+ * Date:26/02/26
+ *REG:RA2411026010279
+ */
+import java.util.*;
 public class PalindromeCheckerApp {
-    public static void main(String args[]){
-        String input="madam";
-        boolean result = check(input, 0, input.length() - 1);
+    public static void main(String[] args){
+        String word="madam";
+        boolean isPalindrome=true;
 
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + result);
-    }
-
-    private static boolean check(String s, int start, int end) {
-
-        if (start >= end) {
-            return true;
+        int n=word.length()-1;
+        int start=0;
+        for(int i=n;i>0;i--)
+        {
+            if(word.charAt(i)!=word.charAt(start))
+            {
+                isPalindrome=false;
+                break;
+            }
+            start++;
         }
-
-        if (s.charAt(start) != s.charAt(end)) {
-            return false;
+        if(isPalindrome)
+        {
+            System.out.println(word+" is a Palindrome");
         }
-
-        return check(s, start + 1, end - 1);
-    }
-}
-
-
-
-
+        else
+        {
+            System.out.println(word+"is not a Palindrome");
+        }
     }
 }
